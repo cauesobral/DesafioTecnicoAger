4f369ace14ad18b74c8ed81417dcaa67252d90ff
caue-sobral <caue-sobral@Orsted>
2026-08-31 00:12:08 -0300

    exception PersistenceException para lançar ao invés de e.printStackTrace

--- src/main/java/br/com/soc/sistema/exception/PersistenceException.java
+++ src/main/java/br/com/soc/sistema/exception/PersistenceException.java
@@ -1,5 +1,5 @@
 package br.com.soc.sistema.exception;
 
 public class PersistenceException {
-
+	//isso vai impedir do dao engolir o erro com o e.printStackTrace, vou fazer dps de terminar os requisitos obritagótios
 }
