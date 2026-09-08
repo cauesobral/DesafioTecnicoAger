467fd12ff678c1e7038d46034e9728690d8905a0
caue-sobral <caue-sobral@Orsted>
2026-09-08 06:31:35 -0300

    Estilização do relatório no excel com cores SOC

--- src/main/java/br/com/soc/sistema/util/RelatorioExcelExporter.java
+++ src/main/java/br/com/soc/sistema/util/RelatorioExcelExporter.java
@@ -6,10 +6,17 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
+import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
 import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFColor;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -18,18 +25,29 @@ import br.com.soc.sistema.vo.CompromissoVo;
 
 public class RelatorioExcelExporter {
 
+	private static final byte[] COR_TURQUESA = {(byte) 0x00, (byte) 0xA3, (byte) 0xA6};
+	private static final byte[] COR_DOURADO = {(byte) 0xFB, (byte) 0xBA, (byte) 0x00};
+	private static final byte[] COR_BRANCO = {(byte) 0xFF, (byte) 0xFF, (byte) 0xFF};
+	private static final byte[] COR_TURQUESA_CLARO = {(byte) 0xE0, (byte) 0xF5, (byte) 0xF5};
+
 	private static final String[] CABECALHO = {
-			"Código Funcionário", "Nome Funcionário", "Código Agenda", "Nome Agenda", "Data", "Hora"
+			"Código Compromisso", "Código Funcionário", "Nome Funcionário",
+			"Código Agenda", "Nome Agenda", "Data", "Hora"
 	};
 
 	public InputStream exportar(List<CompromissoVo> compromissos) {
 		try (XSSFWorkbook workbook = new XSSFWorkbook()) {
 
 			XSSFSheet sheet = workbook.createSheet("Compromissos");
+			sheet.createFreezePane(0, 1);
 
 			CellStyle estiloCabecalho = criarEstiloCabecalho(workbook);
+			CellStyle estiloLinhaPar = criarEstiloLinha(workbook, false);
+			CellStyle estiloLinhaImpar = criarEstiloLinha(workbook, true);
 
 			Row linhaCabecalho = sheet.createRow(0);
+			linhaCabecalho.setHeightInPoints(22);
+
 			for (int i = 0; i < CABECALHO.length; i++) {
 				Cell celula = linhaCabecalho.createCell(i);
 				celula.setCellValue(CABECALHO[i]);
@@ -38,18 +56,27 @@ public class RelatorioExcelExporter {
 
 			int numeroLinha = 1;
 			for (CompromissoVo compromisso : compromissos) {
-				Row linha = sheet.createRow(numeroLinha++);
+				Row linha = sheet.createRow(numeroLinha);
+				CellStyle estiloAtual = (numeroLinha % 2 == 0) ? estiloLinhaPar : estiloLinhaImpar;
 
-				linha.createCell(0).setCellValue(compromisso.getFuncionario().getRowid());
-				linha.createCell(1).setCellValue(compromisso.getFuncionario().getNome());
-				linha.createCell(2).setCellValue(compromisso.getAgenda().getRowid());
-				linha.createCell(3).setCellValue(compromisso.getAgenda().getNome());
-				linha.createCell(4).setCellValue(compromisso.getDataCompromisso().toString());
-				linha.createCell(5).setCellValue(compromisso.getHorarioCompromisso().toString());
+				preencherCelula(linha, 0, compromisso.getRowid(), estiloAtual);
+				preencherCelula(linha, 1, compromisso.getFuncionario().getRowid(), estiloAtual);
+				preencherCelula(linha, 2, compromisso.getFuncionario().getNome(), estiloAtual);
+				preencherCelula(linha, 3, compromisso.getAgenda().getRowid(), estiloAtual);
+				preencherCelula(linha, 4, compromisso.getAgenda().getNome(), estiloAtual);
+				preencherCelula(linha, 5, compromisso.getDataCompromisso().toString(), estiloAtual);
+				preencherCelula(linha, 6, compromisso.getHorarioCompromisso().toString(), estiloAtual);
+
+				numeroLinha++;
+			}
+
+			if (numeroLinha > 1) {
+				sheet.setAutoFilter(new CellRangeAddress(0, numeroLinha - 1, 0, CABECALHO.length - 1));
 			}
 
 			for (int i = 0; i < CABECALHO.length; i++) {
 				sheet.autoSizeColumn(i);
+				sheet.setColumnWidth(i, sheet.getColumnWidth(i) + 1000);
 			}
 
 			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
@@ -62,12 +89,44 @@ public class RelatorioExcelExporter {
 		}
 	}
 
+	private void preencherCelula(Row linha, int coluna, String valor, CellStyle estilo) {
+		Cell celula = linha.createCell(coluna);
+		celula.setCellValue(valor);
+		celula.setCellStyle(estilo);
+	}
+
 	private CellStyle criarEstiloCabecalho(XSSFWorkbook workbook) {
 		Font fonte = workbook.createFont();
 		fonte.setBold(true);
+		fonte.setFontHeightInPoints((short) 11);
+		fonte.setColor(new XSSFColor(COR_BRANCO, null).getIndexed() != -1
+				? IndexedColors.WHITE.getIndex()
+				: IndexedColors.WHITE.getIndex());
 
-		CellStyle estilo = workbook.createCellStyle();
+		XSSFCellStyle estilo = (XSSFCellStyle) workbook.createCellStyle();
 		estilo.setFont(fonte);
+		estilo.setFillForegroundColor(new XSSFColor(COR_TURQUESA, null));
+		estilo.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+		estilo.setAlignment(HorizontalAlignment.CENTER);
+		estilo.setBorderBottom(BorderStyle.MEDIUM);
+		estilo.setBottomBorderColor(new XSSFColor(COR_DOURADO, null));
+		estilo.setBorderTop(BorderStyle.THIN);
+		estilo.setBorderLeft(BorderStyle.THIN);
+		estilo.setBorderRight(BorderStyle.THIN);
+
+		return estilo;
+	}
+
+	private CellStyle criarEstiloLinha(XSSFWorkbook workbook, boolean linhaImpar) {
+		XSSFCellStyle estilo = (XSSFCellStyle) workbook.createCellStyle();
+
+		estilo.setFillForegroundColor(new XSSFColor(linhaImpar ? COR_TURQUESA_CLARO : COR_BRANCO, null));
+		estilo.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+		estilo.setBorderBottom(BorderStyle.THIN);
+		estilo.setBorderTop(BorderStyle.THIN);
+		estilo.setBorderLeft(BorderStyle.THIN);
+		estilo.setBorderRight(BorderStyle.THIN);
+		estilo.setAlignment(HorizontalAlignment.LEFT);
 
 		return estilo;
 	}
